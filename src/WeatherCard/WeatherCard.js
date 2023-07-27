d6a3fd8570afa35883d50f91614a8edbccec2573
Louis Maiorino <lmlmlou@gmail.com>
2023-07-27 19:36:59 -0400

    update react 7-27

--- src/WeatherCard/WeatherCard.js
+++ src/WeatherCard/WeatherCard.js
@@ -1,12 +1,25 @@
-const WeatherCard = () => {
+const weatherOptions = [
+  { url: require("../images/day/sunny.svg").default,day: true, type: "sunny"},
+  { url: require(.."/images/day/cloudy.svg").default,day: true, type: "cloudy"},
+  { url: require(.."/images/night/cloud.svg").default,day: false, type: "cloud"},
+  { url: require(.."/images/night/moon.svg").default,day: false, type: "moon"}
+  ]
 
-  console.log(' weather card') 
+const WeatherCard = ({ day, type }) => {
+  const imageSrc = weatherOptions.filter ((i) => {
+    console.log(i)
+    return i.day === day && i.type === type
+  }
+  console.log(imageSrc)
+  console.log(imageSrc[0].url)
+
+  imageSrcUrl = imageSrc[0].url || '';
   return (
     <section className='weather' id='weather'>
           <div className='weather_info'>75F</div>
-          <img src="/images/day/sunny.svg" className='weather_image'
-        </section>
-  ) 
-}
+          <img src={imageSrcUrl} className='weather_image' />
+    </section>
+  ); 
+};
 
-expect default WeatherCard;
+export default WeatherCard;
