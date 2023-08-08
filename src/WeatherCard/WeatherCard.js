d5d5ce4f709a03b9f8d2a5e8c2abf75eef5f18d8
Louis Maiorino <lmlmlou@gmail.com>
2023-08-07 22:35:38 -0400

    update react 8-7

--- src/WeatherCard/WeatherCard.js
+++ src/WeatherCard/WeatherCard.js
@@ -9,11 +9,11 @@ const WeatherCard = ({ day, type }) => {
   const imageSrc = weatherOptions.filter ((i) => {
     console.log(i)
     return i.day === day && i.type === type
-  }
-  console.log(imageSrc),
+  })
+  console.log(imageSrc);
   console.log(imageSrc[0].url)
 
-  imageSrcUrl = imageSrc[0].url || '';
+  const imageSrcUrl = imageSrc[0].url || '';
   return (
     <section className='weather' id='weather'>
           <div className='weather_info'>75F</div>
