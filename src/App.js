79705aa54ffc6ec30c2c5c0bb0fade5401a79e4e
Louis Maiorino <lmlmlou@gmail.com>
2023-08-20 20:56:51 -0400

    fixed clothing not aliging after text 8-20

--- src/App.js
+++ src/App.js
@@ -50,12 +50,12 @@ function App() {
     <div>
        <Header /> 
       <main className='main'>
-       {/* <section className ="weather" id='weather'> */}
+       
          <WeatherCard day={true} type = 'cloudy' weatherTemp={weatherTemp} />
-        {/* </section> */}
+          <div>Today is {weatherTemp} / You may want to wear:</div>
         <section className ="card_section" id='card-section'>
           <div>
-            Today is {weatherTemp} / You may want to wear:
+            
             {defaultClothingItems.map((item) => {
             return (
               <ItemCard item={item}/>  
