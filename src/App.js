310118880f9832e6c7bffee0e2882305453dfae0
Louis Maiorino <lmlmlou@gmail.com>
2023-08-21 23:11:04 -0400

    added constants.js 8-21

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+
 function App() {
   const weatherTemp = '75°F'
   return (
     <div>
        <Header /> 
-      <Main weatherTemp={weatherTemp}/>       
+      <Main weatherTemp={ weatherTemp }/>       
     </div>
   );
  
