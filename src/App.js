c5c6d4cf614aaa742f50dc764a2bc540c41aca1c
Louis Maiorino <lmlmlou@gmail.com>
2023-08-22 15:49:43 -0400

    video 10 footer error 8-22

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import Footer from './footer/footer';
 
 function App() {
   const weatherTemp = '75°F'
   return (
     <div>
        <Header /> 
-      <Main weatherTemp={ weatherTemp }/>       
+      <Main weatherTemp={ weatherTemp }/>
+      <Footer />     
     </div>
   );
- 
 }
 
 export default App;
