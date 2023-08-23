94c2f29f18ff6369999e211f8a967f4a92677159
Louis Maiorino <lmlmlou@gmail.com>
2023-08-22 20:13:58 -0400

    video part 10 8-22

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
-import Footer from './footer/footer';
+import Footer from './footer/Footer.js';
+import ModalWithForm from './ModalWithForm/ModalWithForm';
 
 function App() {
   const weatherTemp = '75°F'
@@ -10,6 +11,31 @@ function App() {
        <Header /> 
       <Main weatherTemp={ weatherTemp }/>
       <Footer />     
+      <ModalWithForm title="New Garment">
+        <label>
+          Name
+          <input type='text' name='name' minLength={1} maxLength={30}/>
+        </label>
+        <label>
+          Image
+          <input type='url' name='link' minLength={1} maxLength={30}/>
+        </label>
+        <p>Select the weather type:</p>
+        <div>
+          <div>
+            <input type='radio' id='hot' value='hot'/>
+            <label>Hot</label>
+          </div>
+          <div>
+            <input type='radio' id='warm' value='warm'/>
+            <label>Warm</label>
+          </div>
+          <div>
+            <input type='radio' id='cold' value='Cold'/>
+            <label>Cold</label>
+          </div>
+        </div>
+        </ModalWithForm>
     </div>
   );
 }
