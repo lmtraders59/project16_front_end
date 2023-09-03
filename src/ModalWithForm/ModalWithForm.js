fb575686708cbf5ff2aa929e9298db3128c5642f
Louis Maiorino <lmlmlou@gmail.com>
2023-09-03 15:22:35 -0400

    corrected styling for add clothes modal 9-3

--- src/ModalWithForm/ModalWithForm.js
+++ src/ModalWithForm/ModalWithForm.js
@@ -6,11 +6,11 @@ const ModalWithForm = ({ children, buttonText ='Add garment', title,  onClose, n
         <div className={`modal modal_type_${name}`}> 
             <div className="modal__content">
                 <button className ="modal__close" type="button" onClick={onClose}>
-                  Close
+                 <img src={require("../images/close button image.svg").default} alt="close button" />
                 </button> 
                 <h3>{title}</h3>
                 <form>{children}</form>
-                <button type="submit"> {buttonText}</button>
+                <button className="modal__button-text" type="submit"> {buttonText}</button>
             </div>
         </div>
     )
