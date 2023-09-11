d3069442a7f0a52daf4a689473cc8442bb3dcf28
Louis Maiorino <lmlmlou@gmail.com>
2023-09-11 12:23:55 -0400

    corrected clothes modal 9-11

--- src/ItemModal/ItemModal.js
+++ src/ItemModal/ItemModal.js
@@ -3,14 +3,14 @@ const ItemModal = ({ selectedCard, onClose }) => {
  console.log('item modal') 
  return (
     <div className={`modal`}> 
-    <div className="modal__content">
+     <div className="modal__content">
        <button className ="modal__close" type="button" onClick={onClose}>
         <img src={require("../images/close_button.svg").default} alt="close button" />
        </button> 
       <img src={selectedCard.link} alt=''/>
-      <div className="selectedCard">{selectedCard.name}</div>
-      <div>Weather Type: {selectedCard.weather}</div>
-    </div>
+      <div className="modal__selected-card">{selectedCard.name}</div>
+      <div className="modal__weather-type">Weather Type: {selectedCard.weather}</div>
+     </div>
   </div>
  )
 }
