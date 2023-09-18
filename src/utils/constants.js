fc2160e8d38918e79c22bacc279ebf6e8bde6e9a
Louis Maiorino <lmlmlou@gmail.com>
2023-09-18 15:43:14 -0400

    received from code reviwer still have 2 errors 9-17

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -37,9 +37,32 @@ export const defaultClothingItems = [
   },
 ];
 
-export const weatherOptions = {
-  sunny: "../../images/day/sunny.svg",
-  cloudy: "../../images/day/cloudy.svg",
-  cloud: "../../images/night/cloud.svg",
-  moon: "../../images/night/moon.svg"
-}
+// export const weatherOptions = [
+//   sunny: "../../images/day/sunny.svg",
+//   cloudy: "../../images/day/cloudy.svg",
+//   cloud: "../../images/night/cloud.svg",
+//   moon: "../../images/night/moon.svg"
+// ]
+
+export const weatherOptions = [
+    {
+      url: require("../../images/day/sunny.svg").default,
+      day: true,
+      type: "sunny",
+    },
+    {
+      url: require("../../images/day/cloudy.svg").default,
+      day: true,
+      type: "cloudy",
+    },
+    {
+      url: require("../../images/night/cloud.svg").default,
+      day: false,
+      type: "cloud",
+    },
+    {
+      url: require("../../images/night/moon.svg").default,
+      day: false,
+      type: "moon",
+    },
+  ];
