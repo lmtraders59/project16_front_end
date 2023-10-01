ffbaa725737759db98a1b1a2ac9852b080b34850
Louis Maiorino <lmlmlou@gmail.com>
2023-10-01 12:43:49 -0400

    9-30 chaanged favicon logo

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,11 +1,11 @@
-import closebutton from "../../images/close_button.svg";
+import closeButton from "../../images/close_button.svg";
 const ItemModal = ({ selectedCard, onClose }) => {
   console.log("item modal");
   return (
     <div className={`modal`}>
       <div className="modal__content modal__content_type_image">
         <button className="modal__close" type="button" onClick={onClose}>
-          <img src={closebutton} alt="close button" />
+          <img src={closeButton} alt="close button" />
         </button>
         <img src={selectedCard.link} alt={selectedCard.name} />
         <div className="modal__selected-card">{selectedCard.name}</div>
