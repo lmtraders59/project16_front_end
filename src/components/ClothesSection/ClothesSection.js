ff9d65c60b1e422d47f37c58aaaae7ac9c6de3d8
Louis Maiorino <lmlmlou@gmail.com>
2023-12-28 17:27:55 -0500

    12-28 created and fixed clothessection and sidebar

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,4 +1,26 @@
 import "../Profile/Profile.css";
-import "../ItemCard/ItemCard.css";
 import ItemCard from "../ItemCard/ItemCard";
 
+const ClothesSection = ({ onSelectCard, onCreateModal, clothingItems }) => {
+  return (
+    <div className="profile__items">
+      <div className="profile__add-clothes" type="text">
+        <div className="profile__add-new">
+          <p>Your items</p>
+          <div className="profile__new-clothes">
+            <button className="profile__add-button" onClick={onCreateModal}>
+              + Add New
+            </button>
+          </div>
+        </div>
+        <div className="profile__clothes-section">
+          {clothingItems.map((item) => (
+            <ItemCard item={item} onSelectCard={onSelectCard} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+    
+export default ClothesSection;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,34 +1,18 @@
-import ItemCard from "../ItemCard/ItemCard";
-// import avatar from "../../images/Avatar.svg";
 import "./Profile.css";
 import { SideBar } from "../SideBar/SideBar";
+import ClothesSection from "../ClothesSection/ClothesSection";
+ 
 
 const Profile = ({ onSelectCard, onCreateModal, clothingItems }) => {
   return (
     <div className="profile">
-      {/* <div className="profile__sidebar">
-        <img class="profile__avatar-logo" src={avatar} alt="avatar" />
-        <div className="profile__name-person" type="text">
-          Terrence Tegegne
-        </div>
-      </div> */}
-      < SideBar />
+      <SideBar />
+      <ClothesSection 
+      clothingItems={clothingItems}
+      onClick={onCreateModal}
+      onSelectCard={onSelectCard}
+      /> 
       <div className="profile__items">
-        <div className="profile__add-clothes" type="text">
-          <div className="profile__add-new">
-            <p>Your items</p>
-            <div className="profile__new-clothes">
-              <button className="profile__add-button" onClick={onCreateModal}>
-                + Add New
-              </button>
-            </div>
-          </div>
-          <div className="profile__clothes-section">
-            {clothingItems.map((item) => (
-              <ItemCard item={item} onSelectCard={onSelectCard} />
-            ))}
-          </div>
-        </div>
       </div>
     </div>
   );
