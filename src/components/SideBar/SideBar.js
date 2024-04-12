88f267a1b635ea9eafa0fc56f96e31ee64ee2cef
Louis Maiorino <lmlmlou@gmail.com>
2024-04-11 21:37:58 -0400

    4-11 fixed modal signup

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,10 +4,15 @@ import avatar from "../../images/Avatar.svg";
 export function SideBar({ handleLogout }) {
   return (
     <div className="profile__sidebar">
-      <img className="profile__avatar-logo" src={avatar} alt="avatar" />
-      <div className="profile__name-person" type="text">
-        Terrence Tegegne
-      </div>
+      {/* <div className="profile__name-avatar"> */}
+        <img className="profile__avatar-logo" src={avatar} alt="avatar" />
+        <div className="profile__name-person" type="text">
+          Terrence Tegegne
+        </div>
+      {/* </div> */}
+      {/* <div className="sidebar__profile-data"
+        Change profile data
+      </div> */}
       <div className="sidebar__logout" onClick={handleLogout}>
         Log out
       </div>
