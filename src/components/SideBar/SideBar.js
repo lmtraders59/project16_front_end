fb66a339be42506d45b1251354215848cca34d2d
Louis Maiorino <lmlmlou@gmail.com>
2024-04-16 20:52:01 -0400

    4-16 fixed change prodile modal

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,18 +1,15 @@
 import "../Profile/Profile.css";
-import avatar from "../../images/Avatar.svg";
+// import avatar from "../../images/Avatar.svg";
 
 export function SideBar({ currentUser, handleLogout,  handleEditClick }) {
   return (
     <div className="profile__sidebar">
       <div className="profile__name_avatar-section">
-        <img className="profile__avatar-logo" src={avatar} alt="avatar" />
+        <img className="profile__avatar-logo" src={currentUser.avatar} alt="avatar" />
         <div className="profile__name-person" type="text">
-          Terrence Tegegne
+        {currentUser.name}
         </div>
       </div>
-      {/* <div className="sidebar__profile-data"
-        Change profile data
-      </div> */}
       <p className="sidebar__profile-data"  onClick={handleEditClick}>Change profile data</p>
       <p className="sidebar__logout" onClick={handleLogout}>
         Log out
