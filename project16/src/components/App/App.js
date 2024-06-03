cd5f5a0622686f5bdcee9454c4b73d7ad0d80807
Louis Maiorino <louis.maiorino@tappsolutions.com>
2024-06-02 21:59:23 -0400

    6-2 add API key from Google Blogger

--- project16/src/components/App/App.js
+++ project16/src/components/App/App.js
@@ -9,12 +9,12 @@ import NotFound from "../../pages/NotFound";
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={ Home } />
-      <Route exact path="/addEdit" component={ AddEditBlog } />
-      <Route exact path="/editBlog/:id" component={ AddEditBlog } />
-      <Route exact path="/blog/:id" component={ Blog } />
-      <Route exact path="/about" component={ About } />
-      <Route exact path="*" component={ NotFound } />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/addEdit" component={AddEditBlog} />
+      <Route exact path="/editBlog/:id" component={AddEditBlog} />
+      <Route exact path="/blog/:id" component={Blog} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="*" component={NotFound} />
     </div>
   );
 }
---
+++ project16/src/components/utils/ThirdPartyApi.js
@@ -0,0 +1,1 @@
+// API KEY AIzaSyCOILWuoOgG7mZguLKaqglSRfEYHvIQ8xc from Google: https://developers.google.com/blogger/docs/3.0/using
