2622663d91980ffc9c70a8d5fcf1884ec710aa47
Louis Maiorino <louis.maiorino@tappsolutions.com>
2024-06-04 20:28:50 -0400

    6-4

--- project16/src/components/App/App.js
+++ project16/src/components/App/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "../../pages/Home";
 import AddEditBlog from "../../pages/AddEditBlog";
 import Blog from "../../pages/Blog";
@@ -8,14 +8,16 @@ import NotFound from "../../pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/addEdit" component={AddEditBlog} />
-      <Route exact path="/editBlog/:id" component={AddEditBlog} />
-      <Route exact path="/blog/:id" component={Blog} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="*" component={NotFound} />
-    </div>
+    <Switch>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/addEdit" component={AddEditBlog} />
+        <Route exact path="/editBlog/:id" component={AddEditBlog} />
+        <Route exact path="/blog/:id" component={Blog} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="*" component={NotFound} />
+      </div>
+    </Switch>
   );
 }
 
