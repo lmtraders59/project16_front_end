6d3ec5ac51b0d98f4e28f1f3bc6cf0367e6046be
Louis Maiorino <louis.maiorino@tappsolutions.com>
2024-07-29 21:44:04 -0400

    7-29

---
+++ project16/src/components/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,30 @@
+import "./ModalWithForm.css";
+import closeButton from "../../images/close_button.svg";
+const ModalWithForm = ({
+  children,
+  buttonText = "Add garment",
+  title,
+  onClose,
+  name,
+  onSubmit,
+}) => {
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__content">
+        <button className="modal__close" type="button" onClick={onClose}>
+          <img src={closeButton} alt="close button" />
+        </button>
+        <h3 className="modal__form-title">{title}</h3>
+        <form onSubmit={onSubmit}>
+          {children}
+          <button className="modal__submit" type="submit">
+            {" "}
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWithForm;
