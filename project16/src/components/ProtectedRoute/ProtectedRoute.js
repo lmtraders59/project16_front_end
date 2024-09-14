df9b8b147e66f753017edc0634b741b0d7211b39
Louis Maiorino <louis.maiorino@tappsolutions.com>
2024-09-14 10:22:15 -0400

    9-14 submit to reviewer

--- project16/src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Route, Redirect } from "react-router-dom";
-
-function ProtectedRoute({ children, isLoggedIn, ...props }) {
-  return (
-    <Route {...props}>{isLoggedIn ? children : <Redirect to={"/"} />}</Route>
-  );
-}
-
-export default ProtectedRoute;
--- project16/src/pages/SignUp/SignUp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const SignUp = () => {
-  return <h2>Sign Up</h2>;
-};
-
- 
