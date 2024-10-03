67398bc4026e0a8d3a4d6e170982e43723db6bf9
Louis Maiorino <louis.maiorino@tappsolutions.com>
2024-10-03 16:07:59 -0400

    10-3 modal for mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,22 +1,26 @@
 import React from "react";
 
 const MobileMenu = ({ isOpen, onClose }) => (
-  <div className={`mobile-menu ${isOpen ? "mobile-menu_visible" : ""}`}>
-    <div className="close-button" onClick={onClose}>
-      ×
+  <>
+    {isOpen && <div className="modal-overlay" onClick={onClose}></div>}
+    {/* const MobileMenu = ({ isOpen, onClose }) => ( */}
+    <div className={`mobile-menu ${isOpen ? "mobile-menu_visible" : ""}`}>
+      <div className="close-button" onClick={onClose}>
+        ×
+      </div>
+      <ul>
+        <li>
+          <a href="#/">Home</a>
+        </li>
+        <li>
+          <a href="#profile">Profile</a>
+        </li>
+        <li>
+          <a href="#username">Username</a>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li>
-        <a href="#/">Home</a>
-      </li>
-      <li>
-        <a href="#profile">Profile</a>
-      </li>
-      <li>
-        <a href="#username">Username</a>
-      </li>
-    </ul>
-  </div>
+  </>
 );
 
 export default MobileMenu;
